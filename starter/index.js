e593ca640ae4c6aeef13673538e8395c244d321c
Magda <magda@MacBook-Pro-Magda.local>
2022-03-04 01:21:38 +0100

    1-node-farm: export.modules

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,7 @@
 import fs  from 'fs';
 import http from 'http';
 import { URL } from 'url';
+import replaceTemp from './modules/replaceTemp.cjs';
 
 const name = 'Magdalena';
 console.log('name: ', name);
@@ -44,20 +45,6 @@ const cardTemp = fs.readFileSync('./templates/template-card.html', 'utf-8');
 
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
 const productData = JSON.parse(data);
-
-const replaceTemp = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
 
 // ====== SERVER ======
 
